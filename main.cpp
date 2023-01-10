6847ba3eda84d867949932a9774fe4215784df5b
Nate Lalor <natelalor@gmail.com>
2023-01-10 12:14:31 -0500

    finished pngs and implemented them. Tested png/sprite compatibility

--- main.cpp
+++ main.cpp
@@ -1,41 +1,44 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
-int main() {
-    float windowHeight = 400;
-    float windowWidth = 400;
+int main()
+{
+    float windowHeight = 990;
+    float windowWidth = 1400;
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "petals");
-    sf::Texture texture;
-    if (!texture.loadFromFile("images/flower.png")) {
+
+    // Background Rendering Here
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("images/background.png")) {
         return 0;
     }
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
+    sf::Sprite backgroundSprite;
+    backgroundSprite.setTexture(backgroundTexture);
 
+    // Flower Rendering Here
+    sf::Texture flowerTexture;
+    if (!flowerTexture.loadFromFile("images/flower.png"))
+    {
+        return 0;
+    }
+    sf::Sprite flowerSprite;
+    flowerSprite.setTexture(flowerTexture);
 
-    while(window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while(window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
         }
 
         window.clear();
-        window.draw(sprite);
+        window.draw(backgroundSprite);
+        window.draw(flowerSprite);
         window.display();
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
